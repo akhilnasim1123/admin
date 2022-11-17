c4e8883de52024ce8952e88813e4dcf52a821f4c
akhil <akhilnasim029@gmail.com>
2022-11-17 14:29:31 +0530

    payment setted

--- account/views.py
+++ account/views.py
@@ -19,14 +19,13 @@ from wishlist.models import Wishlist
 # Create your views here.
 
 
-
 def landing_page(request):
-    banner          = BannerManagement.objects.all()
+    banner = BannerManagement.objects.all()
 
-    context         = {
-        'banner':banner
+    context = {
+        'banner': banner
     }
-    return render(request, 'landing/index.html',context)
+    return render(request, 'landing/index.html', context)
 
 
 def user_loginpage(request):
@@ -59,12 +58,13 @@ def home(request):
         customer = request.user
         print('customer')
         try:
-            order,create = Order.objects.get_or_create(account=customer, complete=False)
+            order, create = Order.objects.get_or_create(
+                account=customer, complete=False)
             items = order.orderitems_set.all()
             cartItems = order.get_cart_items
             products = Product.objects.all()
             data = {'products': products, 'items': items,
-               'order': order, 'cartItems': cartItems}
+                    'order': order, 'cartItems': cartItems}
             return render(request, 'page.html', data)
         except:
             print("An exception occurred")
@@ -80,11 +80,11 @@ def home(request):
         'products': products,
         'cartItems': cartItems,
         'items': items,
-        'order': order, 
+        'order': order,
     }
     return render(request, 'page.html', data)
 
-    
+
 def login_page(request):
     if request.method == 'POST':
         email = request.POST.get('email')
@@ -100,13 +100,13 @@ def login_page(request):
             messages.error(request, 'Invalid Details')
             return redirect('loginpage')
         elif email == '':
-            messages.error(request,'email is Required')
+            messages.error(request, 'email is Required')
         elif password == '':
-            messages.error(request,'Password is Required')
+            messages.error(request, 'Password is Required')
         else:
             messages.error(request, 'Invalid Details')
             return redirect('loginpage')
-        
+
     return redirect('loginpage')
 
 
@@ -131,17 +131,17 @@ def product_view(request, id):
             items = order.orderitems_set.all()
             cartItems = order.get_cart_items
             val = Product.objects.get(id=id)
-            wishlist=Wishlist.objects.get(Product=val,account=customer)
-            
-            context = {'key5': val, 'items': items,'wishlist':wishlist,
-               'order': order, 'cartItems': cartItems}
+            wishlist = Wishlist.objects.get(Product=val, account=customer)
+
+            context = {'key5': val, 'items': items, 'wishlist': wishlist,
+                       'order': order, 'cartItems': cartItems}
             return render(request, 'product_view.html', context)
         except:
             order = Order.objects.get(account=customer, complete=False)
             items = order.orderitems_set.all()
             val = Product.objects.get(id=id)
             cartItems = order.get_cart_items
-            context = {'key5': val,'cartItems': cartItems}
+            context = {'key5': val, 'cartItems': cartItems}
             print("An exception occurred")
             return render(request, 'product_view.html', context)
 
@@ -156,6 +156,7 @@ def product_view(request, id):
     context = {'key5': val, 'items': items,
                'order': order, 'cartItems': cartItems}
     return render(request, 'product_view.html', context)
+
 
 @never_cache
 def otp_login_page(request):
@@ -224,11 +225,10 @@ class OtpGenerate():
 
 
 @login_required(login_url=login_page)
-def user_profile(request,id):
-   
-   
+def user_profile(request, id):
+
     if request.user.is_authenticated:
-        
+
         print('authenticated')
         customer = request.user
         print('customer')
@@ -237,8 +237,8 @@ def user_profile(request,id):
             items = order.orderitems_set.all()
             cartItems = order.get_cart_items
             datas = {
-                'data':data,
-                 'cartItems': cartItems}
+                'data': data,
+                'cartItems': cartItems}
             return render(request, 'userprofile/profile.html', datas)
         except:
             print("An exception occurred")
@@ -249,17 +249,17 @@ def user_profile(request,id):
     else:
         return redirect(login_page)
 
-
-    data                    = ShippingAddress.objects.filter(account=id).first()
+    data = ShippingAddress.objects.filter(account=id).first()
     cartItems = []
-    datas ={
-        'data':data,
+    datas = {
+        'data': data,
         'cartItems': cartItems,
 
     }
-    return render(request,'userprofile/profile.html',datas)
+    return render(request, 'userprofile/profile.html', datas)
 
-def address_view(request,id):
+
+def address_view(request, id):
     if request.user.is_authenticated:
         print('authenticated')
         customer = request.user
@@ -269,7 +269,7 @@ def address_view(request,id):
             items = order.orderitems_set.all()
             cartItems = order.get_cart_items
             context = {'items': items,
-               'order': order, 'cartItems': cartItems}
+                       'order': order, 'cartItems': cartItems}
             return render(request, 'address_view.html', context)
         except:
             print("An exception occurred")
@@ -279,88 +279,88 @@ def address_view(request,id):
         items = []
         cartItems = []
 
-    account                             = Account.objects.get(id=id)
-    datas                               = ShippingAddress.objects.filter(account=id)
-    context ={
-        'datas':datas,
+    account = Account.objects.get(id=id)
+    datas = ShippingAddress.objects.filter(account=id)
+    context = {
+        'datas': datas,
         'cartItems': cartItems,
         'items': items,
-        'order': order, 
+        'order': order,
     }
-    return render(request,'userprofile/address_view.html',context)
+    return render(request, 'userprofile/address_view.html', context)
 
 
-def delete_address(request,id,user_id):
-    delete                              = ShippingAddress.objects.filter(id=id)
+def delete_address(request, id, user_id):
+    delete = ShippingAddress.objects.filter(id=id)
     delete.delete()
-    return redirect(address_view,user_id)
-def address_edit(request,id):
+    return redirect(address_view, user_id)
+
+
+def address_edit(request, id):
     details = ShippingAddress.objects.get(id=id)
     details.address = request.POST.get('address')
     details.city = request.POST.get('city')
     details.state = request.POST.get('state')
-    details.phone= request.POST.get('phone') 
+    details.phone = request.POST.get('phone')
     details.save()
     return redirect(address_view)
 
-def order_userside(request,id):
-    account              = Account.objects.get(id=id)
-    orders               = OrderedItems.objects.filter(account=account).order_by('id')
+
+def order_userside(request, id):
+    account = Account.objects.get(id=id)
+    orders = OrderedItems.objects.filter(account=account).order_by('id')
     order = Order.objects.get(account=account, complete=False)
     items = order.orderitems_set.all()
-    cartItems = order.get_cart_items   
-    context={
-        'orders':orders,'cartItems':cartItems,
+    cartItems = order.get_cart_items
+    context = {
+        'orders': orders, 'cartItems': cartItems,
     }
-    return render(request,'userprofile/user_orders.html',context)
+    return render(request, 'userprofile/user_orders.html', context)
 
-def add_wishlist(request,product_id,user_id,id):
-    product                         = Product.objects.get(id=product_id)
-    user                            = Account.objects.get(id=user_id)
-    if Wishlist.objects.filter(product=product, account =user).exists():   
-        wishlist                    = Wishlist.objects.filter(product=product,account=user)
+
+def add_wishlist(request, product_id, user_id, id):
+    product = Product.objects.get(id=product_id)
+    user = Account.objects.get(id=user_id)
+    if Wishlist.objects.filter(product=product, account=user).exists():
+        wishlist = Wishlist.objects.filter(product=product, account=user)
         if wishlist:
             wishlist.delete()
             if id == '1':
-                return redirect('product_view',product_id)
+                return redirect('product_view', product_id)
             else:
-                return redirect('wishlists',user_id)
+                return redirect('wishlists', user_id)
         else:
-            return redirect('wishlists',product_id)
+            return redirect('wishlists', product_id)
     else:
-        product                     = Product.objects.get(id=product_id)
-        user                        = Account.objects.get(id=user_id)
-        wishlist                    = Wishlist.objects.create(product=product,account=user)     
-        return redirect('product_view',product_id)
+        product = Product.objects.get(id=product_id)
+        user = Account.objects.get(id=user_id)
+        wishlist = Wishlist.objects.create(product=product, account=user)
+        return redirect('product_view', product_id)
 
-def wishlist_userside(request,id):
+
+def wishlist_userside(request, id):
     # try:
     account = Account.objects.get(id=id)
     wishlst = Wishlist.objects.filter(account=account)
     try:
         order = Order.objects.get(account=account, complete=False)
         items = order.orderitems_set.all()
-        cartItems = order.get_cart_items   
-        context = {'cartItems': cartItems,'wishlst':wishlst}
-        return render(request,'userprofile/wishlist.html',context)
+        cartItems = order.get_cart_items
+        context = {'cartItems': cartItems, 'wishlst': wishlst}
+        return render(request, 'userprofile/wishlist.html', context)
     except:
         print("An exception occurred")
 
-
-    
     print(wishlst)
-    context ={
-        'wishlst':wishlst
+    context = {
+        'wishlst': wishlst
     }
-    return render(request,'userprofile/wishlist.html',context)
+    return render(request, 'userprofile/wishlist.html', context)
     # except:
     #     wishlst = []
     #     context = {
     #         'wishlst':wishlst
     #     }
-
-
-    
 
 
 # def user(request):
--- cart/views.py
+++ cart/views.py
@@ -14,67 +14,74 @@ from proj.models import Order, OrderItems, Product, ShippingAddress
 
 
 def cart(request):
-    
+
     if request.user.is_authenticated:
         print('authenticated')
-        customer                = request.user
+        customer = request.user
         print('customer')
-        order, created          = Order.objects.get_or_create(account=customer, complete=False)
-        items                   = OrderItems.objects.all()
+        order, created = Order.objects.get_or_create(
+            account=customer, complete=False)
+        items = OrderItems.objects.all()
         for i in items:
             print(i.get_total)
-        cartItems               = order.get_cart_items
-        context = {'items': items, 'order': order,'cartItems': cartItems}
+        cartItems = order.get_cart_items
+        context = {'items': items, 'order': order, 'cartItems': cartItems}
         return render(request, 'cart/cart.html', context)
     elif request.user is None:
-        order                   = []
-        items                   = []
-        cartItems               = []
-    order                       = []
-    items                       = []
-    cartItems                   = []
-    context = {'items': items, 'order': order,'cartItems': cartItems}
+        order = []
+        items = []
+        cartItems = []
+    order = []
+    items = []
+    cartItems = []
+    
+    messages.error(request,'Cart is Empty')
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'cart/cart.html', context)
+
 
 @login_required(login_url=login_page)
 def shoppingaddress(request):
     if request.user.is_authenticated:
         print('authenticated')
-        customer                    = request.user
-        order                       = Order.objects.get(account=customer, complete=False)
-        items                       = order.orderitems_set.all()
-        cartItems                   = order.get_cart_items
-        address                     = ShippingAddress.objects.filter(account=customer)
+        customer = request.user
+        order = Order.objects.get(account=customer, complete=False)
+        items = order.orderitems_set.all()
+        cartItems = order.get_cart_items
+        address = ShippingAddress.objects.filter(account=customer).order_by('id')
 
     else:
-        items                       = []
-        order                       = {'get_cart_items': 0, 'get_cart_total': 0, }
-        cartItems                   = order['get_cart_items']
-        address                     = []
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total': 0, }
+        cartItems = order['get_cart_items']
+        address = []
 
         print('in else')
 
-    context = {'items': items, 'order': order, 'cartItems': cartItems,'address':address}
+    context = {'items': items, 'order': order,
+               'cartItems': cartItems, 'address': address}
     return render(request, 'shipping/shipping.html', context)
 
 
 def updateItem(request):
-    data                                = json.loads(request.body)
-    productId                           = data['productId']
-    action                              = data['action']
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
     print('Action:', action)
     print('productId:', productId)
-    account                             = request.user
-    product                             = Product.objects.get(id=productId)
-    order, created                      = Order.objects.get_or_create(account=account, complete=False)
+    account = request.user
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(
+        account=account, complete=False)
 
-    orderItem, created                  = OrderItems.objects.get_or_create(order=order, product=product)
+    orderItem, created = OrderItems.objects.get_or_create(
+        order=order, product=product)
     print('hr')
     if action == 'add':
-        orderItem.quantity              = (orderItem.quantity + 1)
+        orderItem.quantity = (orderItem.quantity + 1)
 
     elif action == 'remove':
-        orderItem.quantity              = (orderItem.quantity - 1)
+        orderItem.quantity = (orderItem.quantity - 1)
 
     orderItem.save()
 
@@ -82,88 +89,95 @@ def updateItem(request):
         orderItem.delete()
     return JsonResponse('Item was Added', safe=False)
 
+
 @login_required(login_url=login_page)
 def pay_page(request):
     if request.user.is_authenticated:
         print('authenticated')
-        customer                        = request.user
-        order                           = Order.objects.get(account=customer,complete=False)
-        items                           = order.orderitems_set.all()
-        cartItems                       = order.get_cart_items
+        customer = request.user
+        order = Order.objects.get(account=customer, complete=False)
+        items = order.orderitems_set.all()
+        cartItems = order.get_cart_items
     else:
         return redirect(login_page)
 
     if request.method == "POST":
+        address = request.POST.get('address')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        pincode = request.POST.get('pincode')
+        account = customer
+        order = order
 
-        add                             = ShippingAddress()
-               
-        add.address                     = request.POST.get('address')
-        add.city                        = request.POST.get('city')
-        add.state                       = request.POST.get('state')
-        add.pincode                     = request.POST.get('pincode')
-        add.account                     = customer
-        add.order                       = order
-        track_no = 'akhil'+ str(random.randint(1111111, 9999999))
+
+
+        
+        track_no = 'akhil' + str(random.randint(1111111, 9999999))
         while Order.objects.filter(tracking_no=track_no) is None:
             track_no = 'akhil' + str(random.randint(1111111, 9999999))
 
-        order.tracking_no                 = track_no
+        order.tracking_no = track_no
 
-        if add.address == '' or add.city == '' or add.state == '' or add.pincode == '':
+        if address== '' or city == '' or state == '' or pincode == '':
             messages.error(request, 'These Fields are Required')
             return redirect('shipping')
-        orItems                         = OrderItems.objects.filter(order=order)
+        orItems = OrderItems.objects.filter(order=order)
 
-        
+
+
+
+        addrs = ShippingAddress.objects.filter(address=address, city=city, account=account).exists()
+        if addrs:
+            messages.error(request,'this address already exist')
+        else:
+            add = ShippingAddress()
+            add.address = address
+            add.city = city
+            add.state = state
+            add.pincode = pincode
+            add.account = customer
+            add.order = order
+            add.save()
+        user_order = OrderedItems()
 
         for item in orItems:
-                user_order                      = OrderedItems()
-                user_order.payment              = request.POST.get('payment_mode')
-                orItemss = item
-                prod_qunt                       = Product.objects.get(id=orItemss.product.id)
 
-                user_order.account              = customer
-                user_order.order                = order
-                user_order.orderitems           = orItemss
-                user_order.shippingaddress      = add
-                user_order.quantity             = orItemss.quantity
-                prod_qunt.quantity              -= user_order.quantity
-                user_order                      = request.POST.get('payment_mode')
-                user_order.price                = order.get_cart_total
-                user_order.product              = prod_qunt
-                user_order.payment_id           = request.POST.get('payment_id')
+            user_order.payment = request.POST.get('payment_mode')
+            orItemss = item
+            prod_qunt = Product.objects.get(id=orItemss.product.id)
 
-            
-                add.save()
-                user_order.save()
+            user_order.account = customer
+            user_order.order = order
+            user_order.orderitems = orItemss
 
-        
-                prod_qunt.save()
+            user_order.quantity = orItemss.quantity
+            prod_qunt.quantity -= user_order.quantity
+            user_order.payment = request.POST.get('payment_mode')
+            user_order.price = order.get_cart_total
+            user_order.product = prod_qunt
+            user_order.payment_id = request.POST.get('payment_id')
 
+            user_order.save()
 
+            prod_qunt.save()
 
-
-        product                         = OrderItems.objects.filter(order=order)
+        product = OrderItems.objects.filter(order=order)
         product.delete()
-        if (user_order.payment == 'Paid by Razorpay' or user_order.payment == 'Paid by Paypal'):
-                    return JsonResponse({'status':'Your order has been placed successfully'})
-
+        if user_order.payment == 'Paid by Razorpay' or user_order.payment == 'Paid by Paypal':
+            return JsonResponse({"status": "Your order has been placed successfully"})
 
         context = {'cartItems': cartItems}
-        return render(request, 'shipping/success.html',context)
-    order                               = Order.objects.get(account=customer, complete=False)
-    if (user_order.payment == 'Paid by Razorpay'):
-                    return JsonResponse({'status':'Your order has been placed successfully'})
-
-    items                               = order.orderitems_set.all()
-    cartItems                           = order.get_cart_items
-    context                             = {'cartItems': cartItems}
-    return render(request, 'shipping/success.html',context)
+        return render(request, 'shipping/success.html', context)
+    order = Order.objects.get(account=customer, complete=False)
+    items = order.orderitems_set.all()
+    cartItems = order.get_cart_items
+    context = {'cartItems': cartItems}
+    return render(request, 'shipping/success.html', context)
 
 
-def deletecart(request,id,value):
+def deletecart(request, id, value):
 
-    delete                              = OrderItems.objects.get(id=id)
+    delete = OrderItems.objects.get(id=id)
 
     delete.delete()
 
@@ -176,29 +190,23 @@ def deletecart(request,id,value):
 def razorpay(request):
     cartitems = Order.objects.get(account=request.user)
     total_price = cartitems.get_cart_total
-    
+
     return JsonResponse({
-        'total_price':total_price
+        'total_price': total_price
     })
+
+
 def success(request):
-    return render(request,'shipping/success.html')
+    return render(request, 'shipping/success.html')
 
+    #     OrderedItems.objects.create(
 
+    # order=order,
+    # product=item.product,
+    # price= order.get_cart_total,
+    # quantity             = item.quantity,
 
+    # tracking_no     = order.tracking_no,
+    # shippingaddress = add,
 
-    
-
-
-
-
-                #     OrderedItems.objects.create(
-                
-                # order=order,
-                # product=item.product,
-                # price= order.get_cart_total,
-                # quantity             = item.quantity,
-                
-                # tracking_no     = order.tracking_no,
-                # shippingaddress = add,
-
-                # )
+    # )
